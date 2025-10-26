44628aa272d0305116b3d8c8cbc27ee785f3d4ad
HeheAbhi <abhishekti5659@gmail.com>
2025-10-26 20:16:54 +0530

    creating frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white">
+        <h1>hello</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
